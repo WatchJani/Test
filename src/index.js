edf64142938d10a89db2b76b24bc5d47a82d82c1
WatchJani <jankokondic84@gmail.com>
2021-01-10 17:02:42 +0100

    my app

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import Navigation from "./components/navigatio"
+import "./Css/index.css"
+import { BrowserRouter, Route } from "react-router-dom"
+import Home from "./components/home"
+import About from "./components/about"
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Navigation />
+                <Route path="/" exact component={Home}></Route>
+                <Route path="/About"  component={About}></Route>
+            </BrowserRouter>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"))
